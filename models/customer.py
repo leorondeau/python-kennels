bb4d52c889a6f88715faeea71e754d2573d27b47
Leo Rondeau <christopherleorondeau@gmail.com>
2021-01-15 13:47:03 -0600

    all elements have class function

---
+++ models/customer.py
@@ -0,0 +1,8 @@
+class Customer():
+
+    def __init_(self, id, email, password, name):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.password = password
+        
---
+++ models/employee.py
@@ -0,0 +1,7 @@
+class Employee():
+
+    def __init__(self, id, name, animal_Id, location_Id)
+        self.id =  id
+        self.name = name 
+        self.animal_id = animal_id
+        self.location_id = location_id
